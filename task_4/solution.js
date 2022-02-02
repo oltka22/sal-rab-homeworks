fb50ea7b6571ce445970183017e489058dd2bce4
Olga Yulkova <oltka22@gmail.com>
2022-02-02 20:57:44 +0300

    Task4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,7 +2,9 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
+    let data = JSON.parse(json);
+    let products = data.products;
+    return products;
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
@@ -16,3 +18,11 @@ function parseProducts(json) {
 // Запишите в переменную length значение свойства products.length
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+function renderProductsCards(json){
+    clearProducts();
+    products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);
+     }
+ }
